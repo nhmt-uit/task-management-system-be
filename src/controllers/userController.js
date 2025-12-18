f7fcf6d97dc9d12b3036ec6a725688a50df3bf2b
nhmt-uit <096666671x@gmail.com>
2025-12-18 14:03:53 +0900

    users api

---
+++ src/controllers/userController.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { registerUser };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+
+// CONFIG ROUTES
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");
